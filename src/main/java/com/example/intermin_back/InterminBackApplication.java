4011fb6b45fe0f4e1eceae9d3fb5ad2c700845b1
TRAORE Mohamed Nour <traoremohamednour@MacBook-Pro-de-TRAORE.local>
2026-01-23 21:37:10 +0100

    premier commit

---
+++ src/main/java/com/example/intermin_back/InterminBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.intermin_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterminBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterminBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/intermin_back/InterminBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.intermin_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterminBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
